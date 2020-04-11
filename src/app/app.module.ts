7ea6e54e8cf73ffa07bc814489dab1eb440368fa
javon-lee <jlee@technoformers.com>
2020-04-11 13:52:41 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { EventsComponent } from './components/events/events.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PrayerRequestComponent } from './components/prayer-request/prayer-request.component';
+import { MediaComponent } from './components/media/media.component';
+import { GiveComponent } from './components/give/give.component';
+import { VideoComponent } from './components/media/components/video/video.component';
+import { EventComponent } from './components/events/components/event/event.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    EventsComponent,
+    FooterComponent,
+    HomeComponent,
+    PrayerRequestComponent,
+    MediaComponent,
+    GiveComponent,
+    VideoComponent,
+    EventComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
