a1458ab60959f31d0802575aa148955ebf8864b8
javonlee22 <45987238+javonlee22@users.noreply.github.com>
2020-06-12 21:13:17 -0400

    Remove unnecessary comments

--- src/app/video-item/video-item.component.ts
+++ src/app/video-item/video-item.component.ts
@@ -15,12 +15,6 @@ export class VideoItemComponent implements OnInit {
   ngOnInit(): void {}
 
   escapeHtml(unsafe: string): string {
-    // return unsafe
-    //   .replace(/&amp;/g, '&')
-    //   .replace(/&lt;/g, '<')
-    //   .replace(/&gt;/g, '>')
-    //   .replace(/&quot;/g, '"')
-    //   .replace(/&#039;/g, "'");
     return unescape(unsafe);
   }
 
