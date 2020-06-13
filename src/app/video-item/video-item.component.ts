0707a880885c37c45022a4bdbab4907fc50c75ec
javon-lee <jlee@technoformers.com>
2020-06-12 20:36:25 -0400

    Added youtube api service

--- src/app/video-item/video-item.component.ts
+++ src/app/video-item/video-item.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-video-item',
   templateUrl: './video-item.component.html',
-  styleUrls: ['./video-item.component.css']
+  styleUrls: ['./video-item.component.css'],
 })
 export class VideoItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() video: any;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  escapeHtml(unsafe: string): string {
+    return unsafe
+      .replace(/&amp;/g, '&')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'");
   }
 
+  convertDate(date: string): string {
+    return formatDate(date, 'MMMM dd, yyyy', 'en-US');
+  }
 }
---
+++ src/app/youtube.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class YoutubeService {
+  apiKey: string = 'AIzaSyCVGI6o9mF4jbC8agjH7kl7LUPe7p7wBlM';
+
+  constructor(public http: HttpClient) {}
+
+  getVideosForChanel(channel, maxResults): Observable<Object> {
+    let url =
+      'https://www.googleapis.com/youtube/v3/search?key=' +
+      this.apiKey +
+      '&channelId=' +
+      channel +
+      '&order=date&part=snippet &type=video,id&maxResults=' +
+      maxResults;
+    return this.http.get(url).pipe(
+      map((res) => {
+        return res;
+      })
+    );
+  }
+}
