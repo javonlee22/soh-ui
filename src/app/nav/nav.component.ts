d16399c200fc07ed1c4c5e1d898b905bcb22a75a
javon-lee <jlee@technoformers.com>
2020-06-13 02:49:51 -0400

    Remove console.log

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -17,7 +17,6 @@ export class NavComponent implements OnInit {
     let loc = url.pop();
     let links = document.querySelectorAll(elem);
     links.forEach((link) => {
-      console.log(link);
       let path = link.getAttribute('href')?.split('/').pop();
       if (path === loc) link.setAttribute('class', 'active');
     });
