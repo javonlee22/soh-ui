544a60b77a3a029df522a85f3c2ab1472c849f68
javonlee22 <javonlee411@gmail.com>
2020-10-11 13:13:33 -0400

    General updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: 'http://45.79.49.140:8080/api',
+  apiHost: 'http://45.79.49.140:8080',
   youtubeApiKey: 'AIzaSyCVGI6o9mF4jbC8agjH7kl7LUPe7p7wBlM',
 };
