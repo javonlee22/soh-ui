18303df1c199ef5d8b6ca62a05c1510ccb7d82bb
javonlee22 <javonlee411@gmail.com>
2020-10-11 11:22:25 -0400

    Updated deploy script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: 'https://www.sohfgc.org/api',
+  apiHost: 'http://45.79.49.140:8080/api',
   youtubeApiKey: 'AIzaSyCVGI6o9mF4jbC8agjH7kl7LUPe7p7wBlM',
 };
